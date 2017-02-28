d92c2256cea86087d5eb4473843ca0467db258c6
Oreki <Oreki@DESKTOP-VHDR95U>
2017-02-28 14:13:30 +0800

    20170228：支持获取卡UID

--- 读卡器YLE-280U功能演示/Form1.Designer.cs
+++ 读卡器YLE-280U功能演示/Form1.Designer.cs
@@ -28,15 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnYLE280_Open = new System.Windows.Forms.Button();
             this.btnYLE280_Reset = new System.Windows.Forms.Button();
             this.btnYLE280_Close = new System.Windows.Forms.Button();
-            this.chkRedLed = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
             this.cbComPortNo = new System.Windows.Forms.ComboBox();
             this.lbInfo = new System.Windows.Forms.ListBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.chkBuzzer = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.chkRedLed = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_GetUID = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnYLE280_Open
@@ -68,17 +75,6 @@
             this.btnYLE280_Close.Text = "串口关闭";
             this.btnYLE280_Close.UseVisualStyleBackColor = true;
             this.btnYLE280_Close.Click += new System.EventHandler(this.btnYLE280_Close_Click);
-            // 
-            // chkRedLed
-            // 
-            this.chkRedLed.AutoSize = true;
-            this.chkRedLed.Location = new System.Drawing.Point(361, 94);
-            this.chkRedLed.Name = "chkRedLed";
-            this.chkRedLed.Size = new System.Drawing.Size(48, 16);
-            this.chkRedLed.TabIndex = 2;
-            this.chkRedLed.Text = "红灯";
-            this.chkRedLed.UseVisualStyleBackColor = true;
-            this.chkRedLed.CheckedChanged += new System.EventHandler(this.chkRedLed_CheckedChanged);
             // 
             // label4
             // 
@@ -117,46 +113,99 @@
             this.lbInfo.Name = "lbInfo";
             this.lbInfo.Size = new System.Drawing.Size(531, 148);
             this.lbInfo.TabIndex = 4;
+            this.lbInfo.SelectedIndexChanged += new System.EventHandler(this.lbInfo_SelectedIndexChanged);
+            this.lbInfo.ValueMemberChanged += new System.EventHandler(this.lbInfo_ValueMemberChanged);
             // 
-            // checkBox1
+            // timer1
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(361, 116);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(48, 16);
-            this.checkBox1.TabIndex = 7;
-            this.checkBox1.Text = "绿灯";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkBuzzer);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.chkRedLed);
+            this.groupBox1.Location = new System.Drawing.Point(14, 57);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(99, 100);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "硬件测试";
             // 
             // chkBuzzer
             // 
             this.chkBuzzer.AutoSize = true;
-            this.chkBuzzer.Location = new System.Drawing.Point(361, 138);
+            this.chkBuzzer.Location = new System.Drawing.Point(16, 64);
             this.chkBuzzer.Name = "chkBuzzer";
             this.chkBuzzer.Size = new System.Drawing.Size(60, 16);
-            this.chkBuzzer.TabIndex = 8;
+            this.chkBuzzer.TabIndex = 11;
             this.chkBuzzer.Text = "蜂鸣器";
             this.chkBuzzer.UseVisualStyleBackColor = true;
-            this.chkBuzzer.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            this.chkBuzzer.CheckedChanged += new System.EventHandler(this.chkBuzzer_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(16, 42);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(48, 16);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "绿灯";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // chkRedLed
+            // 
+            this.chkRedLed.AutoSize = true;
+            this.chkRedLed.Location = new System.Drawing.Point(16, 20);
+            this.chkRedLed.Name = "chkRedLed";
+            this.chkRedLed.Size = new System.Drawing.Size(48, 16);
+            this.chkRedLed.TabIndex = 9;
+            this.chkRedLed.Text = "红灯";
+            this.chkRedLed.UseVisualStyleBackColor = true;
+            this.chkRedLed.CheckedChanged += new System.EventHandler(this.chkRedLed_CheckedChanged_1);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_GetUID);
+            this.groupBox2.Location = new System.Drawing.Point(138, 57);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "序列号";
+            // 
+            // button_GetUID
+            // 
+            this.button_GetUID.Location = new System.Drawing.Point(6, 20);
+            this.button_GetUID.Name = "button_GetUID";
+            this.button_GetUID.Size = new System.Drawing.Size(75, 23);
+            this.button_GetUID.TabIndex = 11;
+            this.button_GetUID.Text = "获取UID";
+            this.button_GetUID.UseVisualStyleBackColor = true;
+            this.button_GetUID.Click += new System.EventHandler(this.button_GetUID_Click);
             // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(590, 562);
-            this.Controls.Add(this.chkBuzzer);
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.cbComPortNo);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.lbInfo);
-            this.Controls.Add(this.chkRedLed);
             this.Controls.Add(this.btnYLE280_Close);
             this.Controls.Add(this.btnYLE280_Reset);
             this.Controls.Add(this.btnYLE280_Open);
             this.Name = "FrmMain";
             this.Text = "功能测试主界面";
             this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -167,12 +216,16 @@
         private System.Windows.Forms.Button btnYLE280_Open;
         private System.Windows.Forms.Button btnYLE280_Reset;
         private System.Windows.Forms.Button btnYLE280_Close;
-        private System.Windows.Forms.CheckBox chkRedLed;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox cbComPortNo;
         private System.Windows.Forms.ListBox lbInfo;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.CheckBox chkBuzzer;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox chkRedLed;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_GetUID;
     }
 }
 
--- 读卡器YLE-280U功能演示/Form1.cs
+++ 读卡器YLE-280U功能演示/Form1.cs
@@ -91,5 +91,74 @@ namespace 读卡器YLE_280U功能演示
             }
             lbInfo.Items.Add("返回值： " + result.ToString());
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /*if (RF_YLE280U.YLE280_GetReaderCode() == -2)
+            {
+                RF_YLE280U.YLE280_GreenLedOn();
+                RF_YLE280U.YLE280_RedLedOff();
+            }
+            else if(RF_YLE280U.YLE280_GetReaderCode() == 0)
+            {
+                RF_YLE280U.YLE280_GreenLedOff();
+                RF_YLE280U.YLE280_RedLedOn();
+            }*/
+        }
+
+        private void chkBuzzer_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkRedLed_CheckedChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_GetUID_Click(object sender, EventArgs e)
+        {
+            Byte[] ReadBuf = new Byte[20];
+            int result = RF_YLE280U.YLE280_GetSerialNo(ReadBuf);
+            if (result == RF_YLE280U.YLE280_OK)
+            {
+                //读取序列号成功，其中数据在ReadBuf中，将ReadBuf中的数据转换成十六进制，并显示到界面上
+                lbInfo.Items.Add("Get UID Succeed.");
+                string s = ReadBuf.ToHexString();
+                string t = string.Empty;
+                for(int i=s.Length-2;i>=0;i-=2)
+                {
+                    t += s.Substring(i, 2);
+                    t += ":";
+                }
+                t = t.Substring(0, t.Length - 1);
+                while(t.StartsWith("00:"))
+                {
+                    t = t.Substring(3);
+                }
+                lbInfo.Items.Add("UID:" + t);
+            }
+            else
+            {
+                //数据读取失败，可将结果显示到界面上
+                lbInfo.Items.Add("Get UID failed! Error " + result);
+            }
+
+        }
+
+        private void lbInfo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbInfo_ValueMemberChanged(object sender, EventArgs e)
+        {
+            
+        }
     }
 }
